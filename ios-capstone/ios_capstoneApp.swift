603234f2d6493faaf0a454d1f563bef0cfb4e382
Akip Maulana <akip.maulana@gmail.com>
2023-04-09 22:38:55 +0700

    Initial Commit

---
+++ ios-capstone/ios_capstoneApp.swift
@@ -0,0 +1,21 @@
+//
+//  ios_capstoneApp.swift
+//  ios-capstone
+//
+//  Created by Akirah Dev on 09/04/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct ios_capstoneApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
